135d9d9fb668fe0b49d5986003c138cd25f9a8bc
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2024-05-28 21:52:57 +0700

    Update category bar for shirts screen

--- lib/screens/shirts_screen.dart
+++ lib/screens/shirts_screen.dart
@@ -5,6 +5,32 @@ class ShirtsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Shirts Screen');
+    return categoryBar();
+  }
+
+  Column categoryBar() {
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(
+        height: 30,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white
+                  ),
+                  onPressed: () => {},
+                  child: const Text('Shirt category')));
+          },
+        ),
+      )
+    ],
+  );
   }
 }
