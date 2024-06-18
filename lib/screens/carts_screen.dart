fb3baf9f993d1fc0f38b772ff0fb7e44945f4a6d
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2024-06-18 08:38:08 +0700

    Update cart screen

--- lib/screens/carts_screen.dart
+++ lib/screens/carts_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tsport_mobile_app/widgets/cart_item.dart';
 
 class CartsScreen extends StatelessWidget {
   const CartsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Carts screen'
+    return const Align(
+      alignment: Alignment.topCenter,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Giỏ hàng',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            SizedBox(height: 20),
+            CartItem(),
+            CartItem(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class CartItem extends StatefulWidget {
+  const CartItem({super.key});
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  int _quantity = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+      child: Card(
+        elevation: 5,
+        child: Row(
+          children: [
+            Image.network(
+                height: 120,
+                width: 120,
+                'https://product.hstatic.net/1000341630/product/hong-nam6745_da517aea17ca4a0491ed4ba8931a6f5a_master.jpg'),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                children: [
+                  const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Áo đấu...',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      )),
+                  const Row(
+                    children: [
+                      Row(
+                        children: [
+                          Text('Size: '),
+                          Text(
+                            'S',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(width: 20),
+                      Row(
+                        children: [
+                          Text('Màu sắc: '),
+                          Text(
+                            'Đỏ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_quantity > 0) {
+                              _quantity--;
+                            }
+                          });
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStateProperty.all<CircleBorder>(
+                            const CircleBorder(),
+                          ),
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.all(
+                                  16)), // adjust padding as needed
+                        ),
+                        child: const Icon(Icons.remove),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        "$_quantity", // Replace with your quantity variable
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _quantity++;
+                          });
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStateProperty.all<CircleBorder>(
+                            const CircleBorder(),
+                          ),
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.all(
+                                  16)), // adjust padding as needed
+                        ),
+                        child: const Icon(Icons.add),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
